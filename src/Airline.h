7b9c000a20840c833f8a958f9ce1228be2389978
AnaSAzevedo (up202108654) <114309554+AnaSAzevedo2003@users.noreply.github.com>
2023-01-05 23:30:01 +0000

    Update Airline.h

--- src/Airline.h
+++ src/Airline.h
@@ -5,20 +5,67 @@
 
 using namespace std;
 
+/**
+ * @brief Classe que representa uma aula
+ */
+
 class Airline {
 
 private:
+    /// Código da companhia aerea
     string code;
+    
+    /// Nome da companhia aerea
     string name;
+    
+    /// Alcunha da companhia aerea
     string callsign;
+    
+    /// País de origem da companhia aerea
     string country;
 
 public:
+    /**
+    * @brief Construtor Default
+    */    
     Airline();
+    
+    /**
+    * @brief Função que cria um novo objeto `Airline` com os valores especificados para os seus campos
+    * Complexidade temporal O(1)
+    * @param code
+    * @param name
+    * @param callsign
+    * @param country
+    */
     Airline(string code, string name, string callsign, string country);
+    
+    /**
+    * @brief Função que retorna o código da companhia aérea
+    * Complexidade temporal O(1)
+    * @return O valor desejado (código da companhia)
+    */
     string get_code() const;
+    
+    /**
+    * @brief Função que retorna o nome da companhia aérea
+    * Complexidade temporal O(1)
+    * @return O valor desejado (nome da companhia)
+    */
     string get_name() const;
+    
+    /**
+    * @brief Função que retorna a alcunha da companhia aérea
+    * Complexidade temporal O(1)
+    * @return O valor desejado (alcunha da companhia)
+    */
     string get_callsign() const;
+    
+    /**
+    * @brief Função que retorna o país de origem da companhia aérea
+    * Complexidade temporal O(1)
+    * @return O valor desejado (país de origem da companhia)
+    */
     string get_country() const;
 
 };
