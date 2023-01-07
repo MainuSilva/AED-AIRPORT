9ddc8a2bc425737d99ea3fe85b388905536ff8bd
AnaSAzevedo (up202108654) <114309554+AnaSAzevedo2003@users.noreply.github.com>
2023-01-07 11:47:05 +0000

    Update CoordDistance.h

--- src/CoordDistance.h
+++ src/CoordDistance.h
@@ -10,36 +10,37 @@
 
 class CoordDistance {
     /**
-     * Calculates the shortest distance between two coordenates.
-     *
-     * @details Time Complexity: O(log(n))
-     *
+     * @brief Calcula a menor distância entre duas coordnadas
+     * @details Time Complexity: O(1)
+     * 
      * @param lat1 Latitude of the first point
      * @param lat2 Latitude of the second point
      * @param long1 Longitude of the first point
      * @param long2 Longitude of the second point
-     * @return Distance between the two points in Kms
+     * @return O valor pretendido (a menor distância entre duas coordnadas)
      */
     static double haversine(double lat1, double lat2, double long1, double long2);
 
 public:
 
     /**
-     * @brief
-     * Time Complexity:
+     * @brief Calcula a distancia entre um aeroporto de origem e um de destino
+     * @details Time Complexity: O(1)
+     * 
      * @param airportSrc
      * @param airportDest
-     * @return
+     * @return O valor pretendido (a distancia entre um aeroporto de origem e um de destino)
      */
     static double haversine(Airport &airportSrc, Airport &airportDest);
 
     /**
-     * @brief
-     * Time Complexity:
+     * @brief Calcula a distancia entre determinadas coordenadas e um aeroporto de destino
+     * @details Time Complexity: O(1)
+     * 
      * @param lat
      * @param lon
      * @param airportDest
-     * @return
+     * @return O valor pretendido (a distancia entre determinadas coordenadas e um aeroporto de destino)
      */
     static double haversine(double lat, double lon, Airport &airportDest);
 };
