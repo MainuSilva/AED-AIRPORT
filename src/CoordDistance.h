88507f7750958f472c9862b8f099396268878e11
Manuel <manuel.maria2003@hotmail.com>
2023-01-08 09:08:55 +0000

    some changes

--- src/CoordDistance.h
+++ src/CoordDistance.h
@@ -10,13 +10,13 @@
 
 class CoordDistance {
     /**
-     * @brief Calcula a menor distância entre duas coordnadas
-     * @details Time Complexity: O(1)
+     * @brief Calcula a menor distância entre duas coordenadas
+     * @details Complexidade temporal: O(1)
      * 
-     * @param lat1 Latitude of the first point
-     * @param lat2 Latitude of the second point
-     * @param long1 Longitude of the first point
-     * @param long2 Longitude of the second point
+     * @param lat1 Latitude do primeiro ponto
+     * @param lat2 Latitude do segundo ponto
+     * @param long1 Longitude do primeiro ponto
+     * @param long2 Longitude do segundo ponto
      * @return O valor pretendido (a menor distância entre duas coordnadas)
      */
     static double haversine(double lat1, double lat2, double long1, double long2);
@@ -25,21 +25,21 @@ public:
 
     /**
      * @brief Calcula a distancia entre um aeroporto de origem e um de destino
-     * @details Time Complexity: O(1)
+     * @details Complexidade temporal: O(1)
      * 
-     * @param airportSrc
-     * @param airportDest
+     * @param airportSrc aeroporto de Origem
+     * @param airportDest aeroporto de Destino
      * @return O valor pretendido (a distancia entre um aeroporto de origem e um de destino)
      */
     static double haversine(Airport &airportSrc, Airport &airportDest);
 
     /**
      * @brief Calcula a distancia entre determinadas coordenadas e um aeroporto de destino
-     * @details Time Complexity: O(1)
+     * @details Complexidade temporal: O(1)
      * 
-     * @param lat
-     * @param lon
-     * @param airportDest
+     * @param lat Latitude
+     * @param lon longitude
+     * @param airportDest aeroporto de destino
      * @return O valor pretendido (a distancia entre determinadas coordenadas e um aeroporto de destino)
      */
     static double haversine(double lat, double lon, Airport &airportDest);
