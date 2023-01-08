a14b82c64a588827dc6dbbd1890a052d91158ce9
AnaSAzevedo (up202108654) <114309554+AnaSAzevedo2003@users.noreply.github.com>
2023-01-08 15:16:21 +0000

    Documentação

--- src/Interface.h
+++ src/Interface.h
@@ -39,11 +39,7 @@ public:
     /**
      * @brief Função que verifica se o utilizador quer de facto sair após ter pressionado "E"
      * @details Complexidade Temporal: O(1)
-<<<<<<< HEAD
-       O loop 'while(true)' será sempre executado apenas uma vez porque as condições desse loop são sempre verdadeiras.
-=======
      *
->>>>>>> a6575d2c172b95ee9eeea43995eaeb1d267160b0
      * @return true se o utilizador pretender sair (pressionando Y) ou false se o utilizador pretender continuar a utilizar o programa (pressionando N)
      */
     bool verifyExit();
@@ -78,19 +74,106 @@ public:
      */
     void goCoords();
 
+    /**
+     * @brief Função que devolve os pontos de articulação do grafo
+     * @details Complexidade Temporal: O(n + m) onde n é o número de nodes no grafo e m o número de edges.
+     */
     void goArticulationP();
+
+    /**
+     * @brief Função que devolve o diametro do grafo
+     * @details Complexidade Temporal: O(n * (n + m)) onde n é o número de nodes no grafo e m o número de edges.
+     */
     void goDiameter();
+
+    /**
+     * @brief Função que cria uma interface para o menu de escolha de apresentação dos melhores aeroportos (mais voos ou mais airlines)
+     * @details Complexidade Temporal: O(1)
+     */
     void goTopAirports();
+
+    /**
+     * @brief Calcula os melhor números de aeroportos
+     * @details Complexidade Temporal: O(n * log(n)) onde n é o número de nodes no grafo.
+     */
     void goMostFlights();
+
+    /**
+     * @brief Calcula os melhor números de aeroportos
+     * @details Complexidade Temporal: O(n * log(n)) onde n é o número de nodes no grafo.
+     */
     void goMostAirlines();
+
+    /**
+     * @brief Função que pede e retorna uma lista com as airlines escolhidas pelo utilizador
+     * @details Complexidade Temporal: O(1)
+     *
+     * @return Lista com os valores desejados
+     */
     list<string> airlines_select();
+
+    /**
+     * @brief Função que imprime as cidades de destino do aeroporto escolhido pelo utilizador
+     * @details Complexidade Temporal: O(n * log(n)) onde n é o número de aeroportos adjacentes ao aeroporto especificado.
+     *
+     * @param airport
+     */
     void cityList(const string& airport);
+
+    /**
+     * @brief Função que imprime as companhias aereas do aeroporto escolhido pelo utilizador
+     * @details Complexidade Temporal: O(n) onde n é o número de nodes no grafo.
+     *
+     * @param airport
+     */
     void airlinesList(const string& airport);
+
+    /**
+     * @brief Função que retorna as companhias aereas de um pais
+     * @details Complexidade Temporal: O(n), onde n é o número de companhias aéreas
+     *
+     * @param country
+     */
     void airlinesListCountry(const string& country);
+
+    /**
+     * @brief Função que retorna todos os paises possíveis de atingir só com um voo
+     * @details Complexidade Temporal: O(n * log(n)) onde n é o número de aeroportos adjacentes ao aeroporto especificado
+     *
+     * @param airport
+     */
     void countriesList(const string& airport);
+
+    /**
+     * @brief Função que retorna todos os aeroportos possíveis de atingir só com um voo
+     * @details Complexidade Temporal: O(n) onde n é o número de aeroportos adjacentes ao aeroporto especificado
+     *
+     * @param airport
+     */
     void airportsList(const string& airport);
+
+    /**
+     * @brief Função que calcula todos os aeroportos possíveis de aceder a partir de um determinado país de origem
+     * @details Complexidade temporal O(n), onde n é o número de nós no grafo
+     *
+     * @param country
+     */
     void AirportsNumCountry(string country);
+
+    /**
+     * @brief Função permite que o utilizador insira o código de várias companhias aereas e armazena-as numa lista
+     * @details Complexidade Temporal: O(n), onde n é o número de companhias aereas escritas pelo utilizador
+     *
+     * @return Lista com os valores desejados
+     */
     list<string> getAirlines();
+
+    /**
+     * @brief Função que retorna o número de voos
+     * @details Complexidade Temporal: O(1)
+     *
+     * @return O valor desejado
+     */
     int getNum();
 
     /**
@@ -102,43 +185,49 @@ public:
     /**
      * @brief Função que retorna os dados de um aeroporto
      * @details Complexidade Temporal: O(1)
+     *
+     * @return O valor desejado
      */
     string getAirport();
     
     /**
      * @brief Função que retorna os dados de uma cidade
      * @details Complexidade Temporal: O(1)
+     *
+     * @return O valor desejado
      */
     string getCity();
     
     /**
      * @brief Função que retorna a latitude de um aeroporto
      * @details Complexidade Temporal: O(1)
+     *
+     * @return O valor desejado (latitude de um aeroporto)
      */
     double getLat();
     
     /**
      * @brief Função que retorna a longitude de um aeroporto
      * @details Complexidade Temporal: O(1)
+     *
+     * @return O valor desejado (longitude de um aeroporto)
      */
     double getLong();
 
 
     /**
-<<<<<<< HEAD
-   * @brief Função que retorna os dados de um país
-   * @details Complexidade Temporal: O(1)
-   */
-=======
      * @brief Função que retorna os dados de um país
      * @details Complexidade Temporal: O(1)
+     *
+     * @return O valor desejado
      */
->>>>>>> a6575d2c172b95ee9eeea43995eaeb1d267160b0
     string getCountry();
 
     /**
      * @brief Função que calcula uma lista de paises, cidades e aeroportos que dá para alcançar de um aeroporto com k voos
      * @details Complexidade Temporal: O((n + m) * 3) onde n é o número de nodes no grafo e m o número de edges
+     *
+     * @param airport
      */
     void airportReach(const string& airport);
 
@@ -157,6 +246,8 @@ public:
     /**
      * @brief Função que devolve ao utilizador um menu de escolha sobre que tipo de voos que processar
      * @details Complexidade Temporal: O(1)
+     *
+     * @return O valor correspondente a esse tipo de voo
      */
     int fromWhere();
 
@@ -199,6 +290,8 @@ public:
     /**
      * @brief Função que devolve o placar de voos
      * @details Complexidade Temporal: O(n), onde n é o número de voos no flight Board
+     *
+     * @param airport
      */
     void flightBoard(const string &airport);
 };
